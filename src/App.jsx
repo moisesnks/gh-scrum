36fdb2743291e315494aee23413b61e0b3582e54
Moisés Leiva Parada <79491798+moisesnks@users.noreply.github.com>
2024-04-24 21:59:57 +0000

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import IssueCard from "./IssueCard/IssueCard.jsx";
 import Avatar from "./Avatar/Avatar.jsx";
-import './App.css'
+import "./App.css";
 
 const issue = {
   Title: "Workflow",
@@ -14,23 +14,39 @@ const issue = {
 };
 
 const asignados = [
-  { "name": "moisesnks", "icon": "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Fmoisesnks.png?alt=media" },
-  { "name": "smnzin", "icon": "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Fsmnzin.jpg?alt=media" },
-  { "name": "juan", "icon": "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Frickroll.jpg?alt=media" },
-  { "name": "héctor salamanca" },
-  { "name": "lucas arancibia" },
+  {
+    name: "moisesnks",
+    icon: "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Fmoisesnks.png?alt=media",
+  },
+  {
+    name: "smnzin",
+    icon: "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Fsmnzin.jpg?alt=media",
+  },
+  {
+    name: "juan",
+    icon: "https://firebasestorage.googleapis.com/v0/b/lumo-ghub.appspot.com/o/public%2Frickroll.jpg?alt=media",
+  },
+  { name: "héctor salamanca" },
+  { name: "lucas arancibia" },
 ];
 
 export default function App() {
   return (
-    <div className="App">
-      <IssueCard issue={issue} isBusy={true} assignedTo={asignados} />
-      <IssueCard issue={issue} />
-      <Avatar capacity={0} data={asignados[0]} />
-      <Avatar capacity={18} data={asignados[1]} />
-      <Avatar capacity={16} data={asignados[2]} />
-      <Avatar capacity={10} data={asignados[3]} />
-      <Avatar capacity={8} data={asignados[4]} />
+    <div className="container">
+      <div className="navbar"></div>
+      <div className="body">
+        <div className="left-side">
+          <IssueCard issue={issue} isBusy={true} assignedTo={asignados} />
+          <IssueCard issue={issue} />
+        </div>
+        <div className="right-side">
+          <Avatar capacity={0} data={asignados[0]} />
+          <Avatar capacity={18} data={asignados[1]} />
+          <Avatar capacity={16} data={asignados[2]} />
+          <Avatar capacity={10} data={asignados[3]} />
+          <Avatar capacity={8} data={asignados[4]} />
+        </div>
+      </div>
     </div>
   );
 }
