8c2879b7b1e51cd9233a2ccbe0c69ac039bc3a94
moisesnks <mleiva@utem.cl>
2024-04-29 05:25:18 -0400

    integrate Filter w/ ListTasks

--- src/Tasks/List/List.jsx
+++ src/Tasks/List/List.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Task from '../Task/Task';
 import useTaskAssignments from '../TaskAssignments';
 import './List.css';
+
+import Filtro from '../../utils/Filtro/Filtro';
+
 const ListTasks = ({ tasks, avatars, updateAvatarScore }) => {
+    const [filtro, setFiltro] = useState("todas");
+
+    const filtrar = (filtro) => {
+        setFiltro(filtro);
+    };
+
     const { updatedTasks, assignAvatarToTask, removeAvatarFromTask } = useTaskAssignments(tasks);
 
     const handleDrop = (taskId, avatarId) => {
@@ -32,10 +41,20 @@ const ListTasks = ({ tasks, avatars, updateAvatarScore }) => {
         }
     };
 
+    // Aplicar el filtro directamente en el mapeo de las tareas
+    const tasksToDisplay = updatedTasks.filter((task) => {
+        if (filtro === "ocupadas") {
+            return task.isBusy;
+        } else if (filtro === "libres") {
+            return !task.isBusy;
+        }
+        return true; // "todas" o filtro no definido
+    });
+
     return (
         <div className="task-list-container">
-            <h2 className="task-list-title">Tareas Pendientes</h2>
-            {updatedTasks.map((task) => (
+            <Filtro onFiltrar={filtrar} />
+            {tasksToDisplay.map((task) => (
                 <Task
                     key={task.id}
                     task={task}
