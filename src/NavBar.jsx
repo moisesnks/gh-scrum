00a54fae9b715df04c6689dd57f8c985c5946250
SmnZin <scarrascoi@utem.cl>
2024-05-28 17:30:08 -0400

    estilos glassmoprhism

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,91 +1,99 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { ArrowIcon } from './Icons';
-import './NavBar.css';
-import { useAuth } from './Hooks/useAuth';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ArrowIcon } from "./Icons";
+import "./NavBar.css";
+import { useAuth } from "./Hooks/useAuth";
 
 const NavBar = () => {
-    const { logout, isAdmin } = useAuth();
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
-    const [arrowColor, setArrowColor] = useState("#CCCCCC");
+  const { logout, isAdmin } = useAuth();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [arrowColor, setArrowColor] = useState("#CCCCCC");
 
-    const location = useLocation();
+  const location = useLocation();
 
-    const handleMouseEnter = () => {
-        setArrowColor("#FFFFFF");
-    };
+  const handleMouseEnter = () => {
+    setArrowColor("#FFFFFF");
+  };
 
-    const handleMouseLeave = () => {
-        setArrowColor("#CCCCCC");
-    };
+  const handleMouseLeave = () => {
+    setArrowColor("#CCCCCC");
+  };
 
-    const toggleMobileMenu = () => {
-        setIsMobileMenuOpen(!isMobileMenuOpen);
-    };
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
-    const toggleSubMenu = () => {
-        setIsSubMenuOpen(!isSubMenuOpen);
-    };
+  const toggleSubMenu = () => {
+    setIsSubMenuOpen(!isSubMenuOpen);
+  };
 
-    const links = [
-        { to: '/home', label: 'Home' },
-        { to: '/home/form', label: 'Crear Tarea', needsAdmin: true },
-        // { to: '/home/kanban', label: 'Kanban' },
-        { to: '/home/metrics', label: 'Métricas', needsAdmin: true },
-        // { to: '/home/teams', label: 'Equipos', needsAdmin: true },
-        { to: '/home/users', label: 'Usuarios', needsAdmin: true },
-        // { to: '/home/profile', label: 'Perfil' },
-        // { to: '/home/about', label: 'Acerca de' },
-        { to: '/home/planning-poker', label: 'Planning Poker' }
-    ];
+  const links = [
+    { to: "/home", label: "Home" },
+    { to: "/home/form", label: "Crear Tarea", needsAdmin: true },
+    // { to: '/home/kanban', label: 'Kanban' },
+    { to: "/home/metrics", label: "Métricas", needsAdmin: true },
+    // { to: '/home/teams', label: 'Equipos', needsAdmin: true },
+    { to: "/home/users", label: "Usuarios", needsAdmin: true },
+    // { to: '/home/profile', label: 'Perfil' },
+    // { to: '/home/about', label: 'Acerca de' },
+    { to: "/home/planning-poker", label: "Planning Poker" },
+  ];
 
-    const filteredLinks = isAdmin ? links : links.filter(link => !link.needsAdmin);
-    const initialLinks = filteredLinks.slice(0, 4);
-    const extraLinks = filteredLinks.slice(4);
+  const filteredLinks = isAdmin
+    ? links
+    : links.filter((link) => !link.needsAdmin);
+  const initialLinks = filteredLinks.slice(0, 4);
+  const extraLinks = filteredLinks.slice(4);
 
-    return (
-        <nav className="navbar">
-            <ul className="navbar-links">
-                {initialLinks.map(link => (
-                    <li key={link.to}>
-                        <Link
-                            to={link.to}
-                            className={location.pathname === link.to ? 'active' : ''}
-                        >
-                            {link.label}
-                        </Link>
-                    </li>
+  return (
+    <nav className="navbar">
+      <ul className="navbar-links">
+        {initialLinks.map((link) => (
+          <li key={link.to}>
+            <Link
+              to={link.to}
+              className={location.pathname === link.to ? "active" : ""}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+        {extraLinks.length > 0 && (
+          <li className="dropdown">
+            <button onClick={toggleSubMenu} className="dropdown-button">
+              <div
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <ArrowIcon color={arrowColor} />
+              </div>
+            </button>
+            {isSubMenuOpen && (
+              <ul className="dropdown-menu">
+                {extraLinks.map((link) => (
+                  <li key={link.to}>
+                    <Link
+                      to={link.to}
+                      className={location.pathname === link.to ? "active" : ""}
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
                 ))}
-                {extraLinks.length > 0 && (
-                    <li className="dropdown">
-                        <button onClick={toggleSubMenu} className="dropdown-button">
-                            <div
-                                onMouseEnter={handleMouseEnter}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <ArrowIcon color={arrowColor} />
-                            </div>
-                        </button>
-                        {isSubMenuOpen && (
-                            <ul className="dropdown-menu">
-                                {extraLinks.map(link => (
-                                    <li key={link.to}>
-                                        <Link
-                                            to={link.to}
-                                            className={location.pathname === link.to ? 'active' : ''}
-                                        >
-                                            {link.label}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </li>
-                )}
-            </ul>
-        </nav>
-    );
-}
+              </ul>
+            )}
+          </li>
+        )}
+        <li>
+          <button onClick={logout}>
+            salir
+            <i className="fa fa-right-from-bracket"></i>
+            </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/Views/Login/LoginForm.jsx
+++ src/Views/Login/LoginForm.jsx
@@ -1,56 +1,57 @@
-import React, { useState } from 'react';
-import { useAuth } from '../../Hooks/useAuth';
-import './LoginForm.css';
-import { Navigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useAuth } from "../../Hooks/useAuth";
+import "./LoginForm.css";
+import { Navigate } from "react-router-dom";
 
 const LoginForm = () => {
-    const { user, loading, error, login } = useAuth(); // Obtén las funciones de autenticación del contexto
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const { user, loading, error, login } = useAuth(); // Obtén las funciones de autenticación del contexto
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    if (user) {
-        // Navegar directamnete a la página de inicio si el usuario ya está autenticado
-        return <Navigate to="/home" />;
-    }
+  if (user) {
+    // Navegar directamnete a la página de inicio si el usuario ya está autenticado
+    return <Navigate to="/home" />;
+  }
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        try {
-            await login(email, password); // Llama a la función de inicio de sesión del contexto
-        } catch (error) {
-        }
-    };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password); // Llama a la función de inicio de sesión del contexto
+    } catch (error) {}
+  };
 
-    return (
-        <div className="login-form-container">
-            <h2 className="login-title">Inicio de Sesión</h2>
-            <form className="login-form" onSubmit={handleLogin}>
-                <div className="form-group">
-                    <label className="form-label">Email:</label>
-                    <input
-                        type="email"
-                        className="form-input"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">Contraseña:</label>
-                    <input
-                        type="password"
-                        className="form-input"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit" className="submit-button" disabled={loading}>
-                    Iniciar Sesión
-                </button>
-            </form>
-            {loading && <p className="loading-text">Cargando...</p>}
-            {error && <p className="error-text">Error: {error}</p>}
-        </div>
-    );
+  return (
+    <div className="container-general">
+      <div className="login-form-container">
+        <h2 className="login-title">Inicio de Sesión</h2>
+        <form className="login-form" onSubmit={handleLogin}>
+          <div className="form-group">
+            <label className="form-label">Email:</label>
+            <input
+              type="email"
+              className="form-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Contraseña:</label>
+            <input
+              type="password"
+              className="form-input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit-button" disabled={loading}>
+            Iniciar Sesión
+          </button>
+        </form>
+        {loading && <p className="loading-text">Cargando...</p>}
+        {error && <p className="error-text">Error: {error}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
