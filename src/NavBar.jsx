09fb720c2151bdfa8668a54558647660f3950f37
Moisés Leiva Parada <79491798+moisesnks@users.noreply.github.com>
2024-05-29 02:09:12 +0000

    navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -28,128 +28,76 @@ const NavBar = () => {
     setIsSubMenuOpen(!isSubMenuOpen);
   };
 
-    const links = [
-        { to: '/home', label: 'Home' },
-        // { to: '/home/kanban', label: 'Kanban' },
-        { to: '/home/metrics', label: 'Métricas' },
-        // { to: '/home/teams', label: 'Equipos', needsAdmin: true },
-        { to: '/home/users', label: 'Usuarios', needsAdmin: true },
-        // { to: '/home/about', label: 'Acerca de' },
-        { to: '/home/asistencia', label: 'Asistencia' },
-        { to: '/home/planning-poker', label: 'Planning Poker' },
-        { to: '/home/profile', label: 'Perfil' },
+  const links = [
+    { to: '/home', label: 'Home' },
+    // { to: '/home/kanban', label: 'Kanban' },
+    { to: '/home/metrics', label: 'Métricas' },
+    // { to: '/home/teams', label: 'Equipos', needsAdmin: true },
+    { to: '/home/users', label: 'Usuarios', needsAdmin: true },
+    // { to: '/home/about', label: 'Acerca de' },
+    { to: '/home/asistencia', label: 'Asistencia' },
+    { to: '/home/planning-poker', label: 'Planning Poker' },
+    { to: '/home/profile', label: 'Perfil' },
 
-        // { to: '/home/retrospective', label: 'Retrospectiva' },
+    // { to: '/home/retrospective', label: 'Retrospectiva' },
 
-    ];
+  ];
 
-    const filteredLinks = isAdmin ? links : links.filter(link => !link.needsAdmin);
-    const n = 7;
-    const initialLinks = filteredLinks.slice(0, n);
-    const extraLinks = filteredLinks.slice(n);
-
-    return (
-
-        <div className="flex jc-sb">
-            <nav className="navbar">
-                <ul className="navbar-links">
-                    {initialLinks.map(link => (
-                        <li key={link.to}>
-                            <Link
-                                to={link.to}
-                                className={location.pathname === link.to ? 'active' : ''}
-                            >
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                    {extraLinks.length > 0 && (
-                        <li className="dropdown">
-                            <button onClick={toggleSubMenu} className="dropdown-button">
-                                <div
-                                    onMouseEnter={handleMouseEnter}
-                                    onMouseLeave={handleMouseLeave}
-                                >
-                                    <ArrowIcon color={arrowColor} />
-                                </div>
-                            </button>
-                            {isSubMenuOpen && (
-                                <ul className="dropdown-menu">
-                                    {extraLinks.map(link => (
-                                        <li key={link.to}>
-                                            <Link
-                                                to={link.to}
-                                                className={location.pathname === link.to ? 'active' : ''}
-                                            >
-                                                {link.label}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </li>
-                    )}
-                </ul>
-            </nav>
-            <div className="navbar logout" onClick={logout}>
-                <i className="fas fa-sign-out-alt" ></i>
-            </div>
-        </div>
-
-    );
-}
-        { to: '/home/planning-poker', label: 'Planning Poker' }
-    ];
-
-  const filteredLinks = isAdmin
-    ? links
-    : links.filter((link) => !link.needsAdmin);
-  const initialLinks = filteredLinks.slice(0, 4);
-  const extraLinks = filteredLinks.slice(4);
+  const filteredLinks = isAdmin ? links : links.filter(link => !link.needsAdmin);
+  const n = 7;
+  const initialLinks = filteredLinks.slice(0, n);
+  const extraLinks = filteredLinks.slice(n);
 
   return (
-    <nav className="navbar">
-      <ul className="navbar-links">
-        {initialLinks.map((link) => (
-          <li key={link.to}>
-            <Link
-              to={link.to}
-              className={location.pathname === link.to ? "active" : ""}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-        {extraLinks.length > 0 && (
-          <li className="dropdown">
-            <button onClick={toggleSubMenu} className="dropdown-button">
-              <div
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+
+    <div className="flex jc-sb">
+      <nav className="navbar">
+        <ul className="navbar-links">
+          {initialLinks.map(link => (
+            <li key={link.to}>
+              <Link
+                to={link.to}
+                className={location.pathname === link.to ? 'active' : ''}
               >
-                <ArrowIcon color={arrowColor} />
-              </div>
-            </button>
-            {isSubMenuOpen && (
-              <ul className="dropdown-menu">
-                {extraLinks.map((link) => (
-                  <li key={link.to}>
-                    <Link
-                      to={link.to}
-                      className={location.pathname === link.to ? "active" : ""}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        )}
-      </ul>
-    </nav>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+          {extraLinks.length > 0 && (
+            <li className="dropdown">
+              <button onClick={toggleSubMenu} className="dropdown-button">
+                <div
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <ArrowIcon color={arrowColor} />
+                </div>
+              </button>
+              {isSubMenuOpen && (
+                <ul className="dropdown-menu">
+                  {extraLinks.map(link => (
+                    <li key={link.to}>
+                      <Link
+                        to={link.to}
+                        className={location.pathname === link.to ? 'active' : ''}
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          )}
+        </ul>
+      </nav>
+      <div className="navbar logout" onClick={logout}>
+        <i className="fas fa-sign-out-alt" ></i>
+      </div>
+    </div>
+
   );
-};
+}
 
 
 export default NavBar;
